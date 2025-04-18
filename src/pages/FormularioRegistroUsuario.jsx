941d191bd211a56148ec9acca700106433c52398
SantiagoPerezI2080 <fredy.perez.i@uniautonoma.edu.co>
2025-04-18 18:02:55 -0500

    Se agrega la estetica del formulario de registro

--- src/pages/FormularioRegistroUsuario.jsx
+++ src/pages/FormularioRegistroUsuario.jsx
@@ -35,16 +35,13 @@ export function FormularioRegistroUsuarios() {
   } = useForm();
   const navigate = useNavigate();
   const params = useParams();
-  // Observar el valor del campo "rol"
   const rol = watch("rol", "");
 
   const onSubmit = handleSubmit(async (data) => {
-    // Si el rol es Vicerrector, forzamos el campo programa a cadena vacía.
     if (data.rol !== "Coordinador") {
       data.programa = "";
     }
 
-    // Si estamos actualizando y el password es vacío (o solo espacios), lo eliminamos.
     if (params.id && (!data.password || data.password.trim() === "")) {
       delete data.password;
     }
@@ -90,123 +87,143 @@ export function FormularioRegistroUsuarios() {
   }, []);
 
   return (
-    <div className="max-w-xl mx-auto">
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          placeholder="Nombres y apellidos"
-          {...register("nombre", { required: true })}
-          className="bg-[#d7e9ff] p-3 rounded-lg block w-full mb-3"
-        />
-        {errors.nombre && <span>El nombre es requerido</span>}
-
-        <input
-          type="email"
-          placeholder="Correo"
-          {...register("correo", {
-            required: "El correo es requerido",
-            pattern: {
-              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-              message: "Ingresa un correo válido",
-            },
-          })}
-          className="bg-[#d7e9ff] p-3 rounded-lg block w-full mb-3"
-        />
-        {errors.correo && <span>{errors.correo.message}</span>}
-
-        <input
-          type="password"
-          placeholder="Contraseña"
-          {...register("password", {
-            required: !params.id ? "El password es requerido" : false,
-          })}
-          className="bg-[#d7e9ff] p-3 rounded-lg block w-full mb-3"
-        />
-        {errors.password && <span>{errors.password.message}</span>}
-
-        {/* Rol */}
-        <select
-          {...register("rol", { required: "El rol es requerido" })}
-          className="bg-[#d7e9ff] p-3 rounded-lg block w-full mb-3"
-        >
-          <option value="" disabled>Seleccione un rol</option>
-          <option value="Administrador">Administrador</option>
-          <option value="Coordinador">Coordinador</option>
-          <option value="Vicerrector">Vicerrector</option>
-        </select>
-        {errors.rol && <span>{errors.rol.message}</span>}
-
-        {/* Programa: solo si rol es Coordinador */}
-        {rol === "Coordinador" && (
-          <select
-            {...register("programa", {
-              required: "El programa es requerido para Coordinador",
-            })}
-            className="bg-[#d7e9ff] p-3 rounded-lg block w-full mb-3"
-          >
-            <option value="">Seleccione un programa</option>
-            {PROGRAMAS_PERMITIDOS.map((prog, index) => (
-              <option key={index} value={prog}>
-                {prog}
-              </option>
-            ))}
-          </select>
-        )}
-        {rol === "Coordinador" && errors.programa && (
-          <span>{errors.programa.message}</span>
-        )}
-
-        {/* Input para is_active */}
-        <div className="flex items-center mb-3">
-          <input
-            type="checkbox"
-            id="is_active"
-            {...register("is_active")}
-            className="mr-2"
-          />
-          <label htmlFor="is_active" className="text-black">
-            Activo
-          </label>
+    <div className="min-h-screen flex flex-col">
+      {/* Header */}
+      <div className="flex w-full">
+        <div className="bg-[#1572E8] text-white py-4 px-4 text-xl font-bold w-1/5 flex flex-col justify-center">
+          <p className="text-lg">Pepito</p>
+          <p className="text-sm">Coordinador</p>
         </div>
 
-        <button className="bg-[#1572E8] text-white p-3 rounded-lg block w-full mt-3">
-          Guardar
-        </button>
-        
-      </form>
+        <div className="bg-gradient-to-r from-[#00498B] to-[#001325] text-white py-8 px-8 text-xl font-bold w-4/5 flex justify-start items-center">
+          <h1 className="text-xl font-semibold">REGISTRO / MODIFICACIÓN DE USUARIOS</h1>
+        </div>
+      </div>
 
-      {params.id && (
-        <div className="flex justify-end">
-          <button
-            className="bg-red-500 p-3 rounded-lg block w-full mt-3"
-            onClick={async () => {
-              const accepted = window.confirm("¿Estas seguro?");
-              if (accepted) {
-                await deleteUsuarios(params.id);
-                toast.success("Usuario eliminado", {
-                  position: "top-right",
-                  style: {
-                    background: "#101010",
-                    color: "#fff",
-                  },
-                });
-                navigate("/lista-usuarios");
-              }
-            }}
-          >
-            Eliminar
-          </button>
+      {/* Contenido principal */}
+      <div className="flex-grow flex items-center justify-center">
+        <div className="bg-[#d7e9ff] w-full max-w-4xl mx-auto px-10 py-16 mt-16 mb-16 rounded-lg shadow-md">
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              placeholder="Nombres y apellidos"
+              {...register("nombre", { required: true })}
+              className="bg-white p-3 rounded-lg block w-full mb-3"
+            />
+            {errors.nombre && <span>El nombre es requerido</span>}
+
+            <input
+              type="email"
+              placeholder="Correo"
+              {...register("correo", {
+                required: "El correo es requerido",
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: "Ingresa un correo válido",
+                },
+              })}
+              className="bg-white p-3 rounded-lg block w-full mb-3"
+            />
+            {errors.correo && <span>{errors.correo.message}</span>}
+
+            <input
+              type="password"
+              placeholder="Contraseña"
+              {...register("password", {
+                required: !params.id ? "El password es requerido" : false,
+              })}
+              className="bg-white p-3 rounded-lg block w-full mb-3"
+            />
+            {errors.password && <span>{errors.password.message}</span>}
+
+            <select
+              {...register("rol", { required: "El rol es requerido" })}
+              className="bg-white p-3 rounded-lg block w-full mb-3"
+            >
+              <option value="" disabled>Seleccione un rol</option>
+              <option value="Administrador">Administrador</option>
+              <option value="Coordinador">Coordinador</option>
+              <option value="Vicerrector">Vicerrector</option>
+            </select>
+            {errors.rol && <span>{errors.rol.message}</span>}
+
+            {rol === "Coordinador" && (
+              <select
+                {...register("programa", {
+                  required: "El programa es requerido para Coordinador",
+                })}
+                className="bg-white p-3 rounded-lg block w-full mb-3"
+              >
+                <option value="">Seleccione un programa</option>
+                {PROGRAMAS_PERMITIDOS.map((prog, index) => (
+                  <option key={index} value={prog}>
+                    {prog}
+                  </option>
+                ))}
+              </select>
+            )}
+            {rol === "Coordinador" && errors.programa && (
+              <span>{errors.programa.message}</span>
+            )}
+
+            <div className="flex items-center mb-3">
+              <input
+                type="checkbox"
+                id="is_active"
+                {...register("is_active")}
+                className="mr-2"
+              />
+              <label htmlFor="is_active" className="text-black">
+                Activo
+              </label>
+            </div>
+
+           <div className="flex justify-end m-2">
+
+           <button className="bg-[#1572E8] text-white px-4 py-2 rounded-lg w-full hover:bg-[#0f5fc7] transition-all duration-300 ">
+              Guardar
+            </button>
+
+           </div>
+          </form>
+
+          {params.id && (
+            <div className="flex justify-end m-2">
+              <button
+                className="bg-red-500 text-white px-4 py-2 rounded-lg w-full hover:bg-red-800[ transition-all duration-300 "
+                onClick={async () => {
+                  const accepted = window.confirm("¿Estas seguro?");
+                  if (accepted) {
+                    await deleteUsuarios(params.id);
+                    toast.success("Usuario eliminado", {
+                      position: "top-right",
+                      style: {
+                        background: "#101010",
+                        color: "#fff",
+                      },
+                    });
+                    navigate("/lista-usuarios");
+                  }
+                }}
+              >
+                Eliminar
+              </button>
+            </div>
+          )}
+
           
         </div>
-                
-      )}
-      <div className="flex justify-center mt-10">
-          <Link to="/gestion-usuarios">
-            <button className="bg-[#1572E8] px-3 py-2 rounded-lg text-white hover:bg-[#0f5fc7] transition-all duration-300">
-              Volver
-            </button>
-          </Link>
-        </div>
+      </div>
+
+      {/* Footer */}
+      <footer className="bg-gradient-to-r from-[#00498B] to-[#001325] text-white py-4 px-8 text-xl font-bold flex justify-end">
+      <Link to="/gestion-usuarios">
+              <button className="bg-[#1572E8] px-3 py-2 rounded-lg text-white hover:bg-[#0f5fc7] transition-all duration-300">
+                Volver
+              </button>
+            </Link>
+      </footer>
     </div>
   );
 }
+
